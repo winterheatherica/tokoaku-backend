ab1cc3658ce15325b32dc552c8dd5f08834be72a
winterheatherica <winterheatherica@gmail.com>
2025-07-18 06:30:37 +0700

    minor fix

--- internal/models/current_event.go
+++ internal/models/current_event.go
@@ -5,7 +5,7 @@ import "time"
 type CurrentEvent struct {
 	ID          uint      `gorm:"primaryKey;autoIncrement" json:"id"`
 	EventTypeID uint      `gorm:"not null" json:"event_type_id"`
-	Start       time.Time `gorm:"not null" json:"start"`
+	Start       time.Time `gorm:"not null" json:"start_at"`
 	CreatedAt   time.Time `gorm:"autoCreateTime" json:"created_at"`
 
 	EventType EventType `gorm:"foreignKey:EventTypeID;references:ID" json:"event_type"`
